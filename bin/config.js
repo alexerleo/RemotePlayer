3475c6979ff989ff3c5b2b5b56cb677b4175a38a
alexerleo <alexerleo@gmail.com>
2016-06-08 19:52:58 +0300

    Change db settings

--- bin/config.js
+++ bin/config.js
@@ -3,11 +3,11 @@ config.secret_key = 'secret_key';
 
 // mongodb configuration
 config.mongodb = {};
-config.mongodb.host = process.env.MONGODB_HOST || '127.0.0.1';
-config.mongodb.port = process.env.MONGODB_PORT || 27017;
-config.mongodb.user = process.env.MONGODB_USERNAME || 'remotePlayerAdmin';
-config.mongodb.password = process.env.MONGODB_PASSWORD || 'acIm1n';
-config.mongodb.database = process.env.MONGODB_DATABASE || 'remote';
-config.mongodb.random = process.env.MONGODB_RANDOM || "no_sync";
+config.mongodb.host = 'ds011024.mlab.com';
+config.mongodb.port = 11024;
+config.mongodb.user = 'RemotePlayerAdmin';
+config.mongodb.password = 're^^oTeAclm1n';
+config.mongodb.database = 'remoteplayerhostdb';
+config.mongodb.random = "no_sync";
 
 module.exports = config;
